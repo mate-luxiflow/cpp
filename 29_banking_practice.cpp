b0ba889c0ba8c2f12ed40a472185e0b56b3b9246
mate-luxiflow <asdfgh402317@gmail.com>
2026-08-04 12:00:40 +0200

    29es lecke kondi utan next, 30as utan practice programok

---
+++ 29_banking_practice.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <iomanip>
+
+void showbalance(double balance);
+double deposit();
+double withdraw(double balance);
+
+int main()
+{
+
+    double balance = 124;
+    int choice = 0;
+
+    do
+    {
+        std::cout << "enter your choice: \n";
+        std::cout << "1, show balance\n";
+        std::cout << "2, deposit money\n";
+        std::cout << "3, withdraw money\n";
+        std::cout << "4, exit\n";
+        std::cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            showbalance(balance);
+            break;
+        case 2:
+            balance += deposit();
+            showbalance(balance);
+            break;
+        case 3:
+            balance -= withdraw(balance);
+            showbalance(balance);
+            break;
+        case 4:
+            std::cout << "thanks for visiting\n";
+            break;
+        default:
+            std::cout << "invalid choice\n";
+            break;
+        }
+    } while (choice != 4);
+
+    return 0;
+}
+
+void showbalance(double balance)
+{
+    std::cout << "your balance is: $" << std::setprecision(2) << std::fixed << balance << '\n';
+}
+double deposit()
+{
+    double amount = 0;
+
+    std::cout << "enter the amount to be deposited: ";
+    std::cin >> amount;
+
+    if (amount > 0)
+    {
+        return amount;
+    }
+    else
+    {
+        std::cout << "thats not a valid amount\n";
+        return 0;
+    }
+
+    return amount;
+}
+double withdraw(double balance)
+{
+    double amount = 0;
+
+    std::cout << "enter the amount to be withdrawn\n";
+    std::cin >> amount;
+
+    if (amount > balance)
+    {
+        std::cout << "insufficient funds";
+        return 0;
+    }
+
+    return amount;
+}
